11af0776d56475483acbf87f0f391ce4c871bccf
mac <mac@macbook-pro-mac-2.local>
2019-02-23 16:57:02 +0300

    Project is ready

--- Apple Pie/Controller/ViewController.swift
+++ Apple Pie/Controller/ViewController.swift
@@ -19,10 +19,21 @@ class ViewController: UIViewController {
     // MARK: - ... Properties
     var currentGame: Game!
     
+    var finished = false
+    
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
 
     // MARK: - ... Methods
     override func viewDidLoad() {
@@ -40,8 +51,41 @@ class ViewController: UIViewController {
         }
     }
     
+    func enable(view: UIView, isEnabled: Bool) {
+        if let button = view as? UIButton {
+            button.isEnabled = isEnabled
+        }
+        
+        for subview in view.subviews {
+            enable(view: subview, isEnabled: isEnabled)
+        }
+    }
+    
     func newRound() {
-        let newWord = Game.listOfWords.removeFirst()
+        let count = Game.listOfWords.count
+        
+        guard 0 < count else {
+            correctWordLabel.text = "Игра окончена"
+        
+            enable(view: view, isEnabled: false)
+            
+            finished = true
+            return
+        }
+        
+        enable(view: view, isEnabled: true)
+        
+        let index: Int
+    
+        if count < 2 {
+            index = 0
+        } else {
+            index = Int.random(in:0 ..< count)
+        }
+    
+    
+    
+        let newWord = Game.listOfWords.remove(at: index)
         
         currentGame = Game(
             word: newWord,
@@ -53,17 +97,34 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        guard !finished else { return }
+        
         let imageName = "Tree \(currentGame.incorrectMovesRemaining)"
         let image = UIImage(named: imageName)
         
         treeImageView.image = image
         
+        correctWordLabel.text = currentGame.correctWord
+        
         scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
     }
     
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining < 1 {
+            totalLosses += 1
+        } else if currentGame.correctWord == currentGame.word.lowercased() {
+            totalWins += 1
+        }
+        updateUI()
+    }
     // MARK: - ... IBAction
     @IBAction func buttonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        
+        let letter = sender.title(for: .normal)!
+        
+        currentGame.guess(letter: letter)
+        
+        updateGameState()
     }
 }
-
